76afe2e26f621f4b8f2b7ab6a9b656a56ae1938e
khian calderon <ckhianvictory@gmail.com>
2025-08-03 14:01:55 +0800

    Updated REST API route

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,13 +1,13 @@
-import { NextRequest, NextResponse } from "next/server";
-import { cors } from "@/lib/cors";
+import { NextRequest, NextResponse } from 'next/server'
+import { cors } from '@/lib/cors'
 
 export async function GET(req: NextRequest) {
-    let res = NextResponse.json({
+    const res = NextResponse.json({ 
         message: "Fetch success!"
-    });
-    return cors(req, res);
+    })
+    return cors(req, res)
 }
 
 export async function OPTIONS(req: NextRequest) {
-    return cors(req, new NextResponse());
+    return cors(req, new NextResponse())
 }
