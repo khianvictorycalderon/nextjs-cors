5bc00e875c86709b303d9d6881473edf397a84a5
khian calderon <ckhianvictory@gmail.com>
2025-08-03 14:09:16 +0800

    Added POST route + status codes

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,7 +4,22 @@ import { cors } from '@/lib/cors'
 export async function GET(req: NextRequest) {
     const res = NextResponse.json({ 
         message: "Fetch success!"
+    }, {
+        status: 200
     })
+    return cors(req, res);
+}
+
+export async function POST(req: NextRequest) {
+    const body = await req.json(); // JSON middleware
+    const { name } = body;
+    console.log(`Name received: ${name}`);
+
+    const res = NextResponse.json({
+        message: "Name received at the server!"
+    }, {
+        status: 200
+    });
     return cors(req, res);
 }
 
