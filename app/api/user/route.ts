92b30699fdc710fca9439961feb4106ec2ec6818
khian calderon <ckhianvictory@gmail.com>
2025-08-03 13:55:12 +0800

    Added GET route

---
+++ app/api/user/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cors } from "@/lib/cors";
+
+export async function GET(req: NextRequest) {
+    let res = NextResponse.json({
+        message: "Fetch success!"
+    });
+    return cors(req, res);
+}
+
+export async function OPTIONS(req: NextRequest) {
+    return cors(req, new NextResponse());
+}
