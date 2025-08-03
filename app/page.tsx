c0f0166886d3bc43bfcbc49c068774738076f8f2
khian calderon <ckhianvictory@gmail.com>
2025-08-03 13:07:24 +0800

    Updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-semibold">Home Page</h1>
+        <p className="text-sm">To get started, access the sample REST 'api/user' routes.</p>
+      </div>
+    </div>
   )
 }
